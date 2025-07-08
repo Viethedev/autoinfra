27f03298f0923d0375b3250215dc8d995f8921a6
Viethedev <k51a3-44-viet@thptkimlien-hanoi.edu.vn>
2025-07-08 17:21:49 +0700

    Day 1 update

--- src/deep-learning/numerical/buffer.hpp
+++ src/deep-learning/numerical/buffer.hpp
@@ -2,23 +2,39 @@
 #include <vector>
 #include <memory>
 
-namespace numerical {
+namespace numerical
+{
 
-template<typename T>
-class Buffer {
-public:
-    virtual T* data() = 0;
-    virtual const T* data() const = 0;
-    virtual ~Buffer() = default;
-};
+    template <typename T>
+    class Buffer
+    {
+    public:
+        virtual T *data() = 0;
+        virtual const T *data() const = 0;
+        virtual ~Buffer() = default;
+    };
 
-template<typename T>
-class CpuBuffer : public Buffer<T> {
-    std::vector<T> storage_;
-public:
-    CpuBuffer(size_t size) : storage_(size) {}
-    T* data() override { return storage_.data(); }
-    const T* data() const override { return storage_.data(); }
-};
+    template <typename T>
+    class CpuBuffer : public Buffer<T>
+    {
+        std::vector<T> storage_;
+
+    public:
+        CpuBuffer(size_t size) : storage_(size) {}
+        T *data() override { return storage_.data(); }
+        const T *data() const override { return storage_.data(); }
+    };
+
+    template <typename T>
+    class GpuBuffer : public Buffer<T>
+    {
+    protected:
+        size_t size_;
+        int device_id_;
+
+    public:
+        GpuBuffer(size_t size, int device_id)
+            : size_(size), device_id_(device_id) {}
+    };
 
 }
--- src/deep-learning/numerical/device.hpp
+++ src/deep-learning/numerical/device.hpp
@@ -1,8 +1,28 @@
-#pragma once
+namespace numerical
+{
 
-namespace numerical {
-enum class Device {
-    CPU,
-    GPU
-};
+    enum class DeviceType
+    {
+        CPU,
+        CUDA,
+        ROCm,
+        Vulkan,
+        Metal
+    };
+
+    struct Device
+    {
+        bool is_gpu;
+        int device_id;
+        DeviceType device_type;
+
+        static Device CPU() { return {false, 0, DeviceType::CPU}; }
+        static Device GPU(int id, DeviceType type) { return {true, id, type}; }
+    };
+
 }
+/* Use:
+Device cpu = Device::CPU();
+Device cuda_gpu0 = Device::GPU(0, GpuType::CUDA);
+Device amd_gpu1  = Device::GPU(1, GpuType::ROCm);
+*/
